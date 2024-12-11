46280cf880e5f7f0f77a591a01cc1a882c995def
PRAMODI P.V.A.T <pramodi-im21018@stu.kln.ac.lk>
2024-12-11 11:55:41 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\pramo\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\pramo\\Desktop\\flutter\\calculator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\pramo\\flutter"
+  "PROJECT_DIR=C:\\Users\\pramo\\Desktop\\flutter\\calculator"
+  "FLUTTER_ROOT=C:\\Users\\pramo\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\pramo\\Desktop\\flutter\\calculator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\pramo\\Desktop\\flutter\\calculator"
+  "FLUTTER_TARGET=C:\\Users\\pramo\\Desktop\\flutter\\calculator\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\pramo\\Desktop\\flutter\\calculator\\.dart_tool\\package_config.json"
+)
